d99d45fefa7cc09755245b920324a8e2f0a305db
Jesús Heredia <jes.heredia@gmail.com>
2015-11-19 18:05:11 +0100

    The 'first_time' variable has been removed: it is not necessary to determine the candidate images to be swapped.
    The 'if (images.length > 0)' conditional has been removed as well: it is
    not necessary because if there is no candidate image to be swapped, the
    'swappable_images' variable will be empty and the each loop will not do
    anything with it.
    
    All the variables (with the exception of $win and th) have been declared
    within the Flojo() function (local variables). Local variables are deleted
    when the function is completed.
    
    Finally, it has been added/modified some comments to make the script more
    understandable.

--- js/flojo.js
+++ js/flojo.js
@@ -12,6 +12,7 @@
  * http://creativecommons.org/licenses/by-sa/3.0/
  */
 
+// When the document is ready.
 $(document).ready(function() {
 
   // All the code should be executed in 'strict mode'.
@@ -23,73 +24,57 @@ $(document).ready(function() {
   // Threshold (in pixels). The number of pixels as a minimum between our
   // current position and the following image(s) to be loaded lazily.
   // 200 is the value by default, but you can use the one you like the most.
-  th = 200,
-
-  // Is this the first time that flojo() is called by another piece of code?
-  // True is the value by default.
-  first_time = true,
-
-  // Set of images to be swapped.
-  images = [];
+  th = 200;
 
   function flojo() {
 
-    if (first_time === false) {
+    // Array of candidate images to be swapped.
+    var swappable_images = $('img:not([data-src="swapped"])'),
 
-      images = $('img:not([data-src="swapped"])');
+    // Height of the window.
+    win_height = $win.height(),
 
-    } else if (first_time === true) {
+    // Scrolling from the top of the page.
+    win_scroll_top = $win.scrollTop();
 
-      images = $('img[data-src]');
+    // Iterates through the candidate images.
+    swappable_images.each(function() {
 
-      first_time = false;
+      // Height of the image that is being processing at the moment.
+      var img_height = $(this).height(),
 
-    }
+      // How far (in pixels) this image is from the top of the document.
+      img_offset_top = $(this).offset().top;
 
-    // Is there at least one image to be swapped?
-    if (images.length > 0) {
+      // Loads the candidate images into the HTML document if necessary.
+      if (img_offset_top + img_height >= win_scroll_top - th &&
+          img_offset_top <= win_height + win_scroll_top + th) {
 
-      // The height of the window.
-      var  win_height = $win.height(),
+        var new_image = new Image();
 
-      // Scrolling from the top of the page.
-      win_scroll_top = $win.scrollTop();
+        new_image.src = $(this).attr('data-src');
 
-      images.each(function() {
+        $(this).attr({
 
-        // The height of this image.
-        var img_height = $(this).height(),
+          src: new_image.src,
 
-        // How far (in pixels) this image is from the top of the document.
-        img_offset_top = $(this).offset().top;
+          'data-src': 'swapped'
 
-        if (img_offset_top + img_height >= win_scroll_top - th &&
-            img_offset_top <= win_height + win_scroll_top + th) {
+        });
 
-          var new_image = new Image();
+      }
 
-          new_image.src = $(this).attr('data-src');
-
-          $(this).attr({
-
-            src: new_image.src,
-
-            'data-src': 'swapped'
-
-          });
-
-        }
-
-      });
-
-    }
+    });
 
   } // End of the flojo() function.
 
+  // Call Flojo right after the document is ready.
   flojo();
 
+  // Call Flojo whenever a scroll event occurs.
   $win.scroll(flojo);
 
+  // Call Flojo whenever a resize event occurs.
   $win.resize(flojo);
 
 });
